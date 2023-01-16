fb70e32129f0e5442e34c73ed4d49dc47cb82625
Adnan <adbak.dev@gmail.com>
2023-01-16 20:36:04 +0100

    first commit

---
+++ 001/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contact contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	fmt.Printf("%+v", jim)
+}
